43e0e0f27a15abe176bc376c903513524300d27e
Jasper-3 <118008174+Jasper-3@users.noreply.github.com>
2025-02-11 21:31:37 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+Route::view('/{any?}', 'welcome')->where('any', '.*');
+
+require __DIR__ . '/auth.php';

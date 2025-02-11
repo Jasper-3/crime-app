5808cb83bf7c9d52db01b49a4697d109df9a1f49
Jasper-3 <118008174+Jasper-3@users.noreply.github.com>
2025-02-11 19:48:16 +0800

    breeze

---
+++ app/Http/Controllers/Api/CrimeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CrimeRequest;
+use App\Http\Resources\CrimeResource;
+use App\Models\CrimeSceneReport;
+use Illuminate\Http\Request;
+
+class CrimeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CrimeResource::collection(CrimeSceneReport::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CrimeRequest $request)
+    {
+        $crime = CrimeSceneReport::create($request->validated());
+        return new CrimeResource($crime);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CrimeSceneReport $crime)
+    {
+        return new CrimeResource($crime);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CrimeRequest $request, CrimeSceneReport $crime)
+    {
+        $crime->update($request->validated());
+        return new CrimeResource($crime);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CrimeSceneReport $crime)
+    {
+        $crime->delete();
+        return response()->noContent();
+    }
+}
---
+++ app/Models/CrimeSceneReport.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CrimeSceneReport extends Model
+{
+    use HasFactory;
+    protected $table = "crime_scene_reports";
+
+    protected $fillable = [
+        "year",
+        "month",
+        "day",
+        "street",
+        "description",
+    ];
+}
